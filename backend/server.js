9c51813e962a51b0cb7f74c96ea0015b77f0b674
Haziq Fayaz <ratherhaziq123@gmail.com>
2026-09-18 23:43:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,8 +39,8 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on port ${PORT}`);
 });
