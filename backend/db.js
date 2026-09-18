2c602baffae16abe8a803bc3ce2b6102284a68f2
Haziq Fayaz <ratherhaziq123@gmail.com>
2026-09-18 23:53:35 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,25 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+let db;
+
+if (process.env.DATABASE_URL) {
+  db = mysql.createConnection(process.env.DATABASE_URL);
+} else {
+  db = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "HAZIQ@13",
     database: "StudentHub"
-});
+  });
+}
 
 db.connect((err) => {
-    if (err) {
-        console.log("MySQL connection failed:", err.message);
-        return;
-    }
+  if (err) {
+    console.log("MySQL connection failed:", err.message);
+    return;
+  }
 
-    console.log("MySQL connected successfully!");
+  console.log("MySQL connected successfully!");
 });
 
 module.exports = db;
